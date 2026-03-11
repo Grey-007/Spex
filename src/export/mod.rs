444fe726ef263f9170ae6721adf1c0d2121545fe
Grey-007 <rudra8th@gmail.com>
2026-03-12 01:18:41 +0530

    Format export path helper module

--- src/export/mod.rs
+++ src/export/mod.rs
@@ -6,10 +6,14 @@ use std::io;
 use std::path::PathBuf;
 
 pub fn export_file_path(filename: &str) -> io::Result<PathBuf> {
-    let home = std::env::var("HOME")
-        .map_err(|_| io::Error::new(io::ErrorKind::NotFound, "HOME environment variable not set"))?;
+    let home = std::env::var("HOME").map_err(|_| {
+        io::Error::new(io::ErrorKind::NotFound, "HOME environment variable not set")
+    })?;
 
-    let export_dir = PathBuf::from(home).join(".config").join("spex").join("export");
+    let export_dir = PathBuf::from(home)
+        .join(".config")
+        .join("spex")
+        .join("export");
     std::fs::create_dir_all(&export_dir)?;
 
     Ok(export_dir.join(filename))
