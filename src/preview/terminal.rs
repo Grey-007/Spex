44c06d6990388569e69d261b2ef95d65e051c558
Grey-007 <rudra8th@gmail.com>
2026-03-12 00:45:40 +0530

    Add terminal palette preview module

---
+++ src/preview/terminal.rs
@@ -0,0 +1,24 @@
+use crate::models::color::Color;
+
+const COLORS_PER_ROW: usize = 8;
+
+pub fn print_palette(colors: &[Color]) {
+    println!("Palette ({} colors)", colors.len());
+    println!();
+
+    for (idx, color) in colors.iter().enumerate() {
+        print!("\x1b[48;2;{};{};{}m    \x1b[0m", color.r, color.g, color.b);
+        if (idx + 1) % COLORS_PER_ROW == 0 {
+            println!();
+        }
+    }
+
+    if !colors.len().is_multiple_of(COLORS_PER_ROW) {
+        println!();
+    }
+
+    println!();
+    for color in colors {
+        println!("#{:02X}{:02X}{:02X}", color.r, color.g, color.b);
+    }
+}
