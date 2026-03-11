e380d26405f415a5aaf01a6cc2b8fc9d997caaf6
Grey-007 <rudra8th@gmail.com>
2026-03-12 00:07:43 +0530

    Wire k-means extraction into CLI output

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::error::Error;
 
 use ::image::GenericImageView;
 
+use crate::extract::kmeans::extract_palette;
 use crate::extract::sampler::sample_pixels;
 use crate::image::loader::load_image;
 
@@ -26,12 +27,17 @@ fn run() -> Result<(), Box<dyn Error>> {
 
     let pixels = load_image(&path)?;
     let sampled_pixels = sample_pixels(&pixels, 100);
+    let palette = extract_palette(&sampled_pixels, 8);
 
     println!("Image loaded");
     println!("Width: {width}");
     println!("Height: {height}");
     println!("Original pixel count: {}", pixels.len());
     println!("Sampled pixel count: {}", sampled_pixels.len());
+    println!("Dominant colors:");
+    for color in &palette {
+        println!("#{:02X}{:02X}{:02X}", color.r, color.g, color.b);
+    }
 
     Ok(())
 }
